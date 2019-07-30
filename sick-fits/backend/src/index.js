f84d56eb98d9b55d5a37d8c36fdcc45ee51323df
Jonne Kanerva <jonne.kanerva@siili.com>
2019-07-30 14:37:12 +0300

    Video 20 start

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer')
+
+const db = require('./db')
+
+const server = createServer()
+
+// TODO User express middleware to handle cookies
+// TODO User express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, deets => {
+  console.log(`Server is now running in port http://localhost:${deets.port}`)
+})
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check login
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item
+  },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id; // Do not update the ID
+    return await ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
+  }
+};
 
 module.exports = mutations;
